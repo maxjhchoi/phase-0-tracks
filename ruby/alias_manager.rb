3202af2cbd47c82ac94a74c946e303d890fa70b4
maxjhchoi <max.jh.choi@gmail.com>
2016-12-20 17:52:13 -0500

    Update alias_manager.rb

---
+++ ruby/alias_manager.rb
@@ -0,0 +1,13 @@
+
+#Enter name
+name = "max choi"
+
+#Spell name backwards
+p name_list = name.split(" ")
+p name_backwards = name_list.reverse
+
+#Create arrays of vowels and consonants
+vowels = ["a", "e", "i", "o", "u"]
+consonants = ["b", "c", "d", "f", "g", "h", "j", "k", "l", "m", "n", "p", "q", "r", "s", "t", "v", "w", "x", "y", "z"]
+
+#Got stuck, but handing in what I have so far in order to complete week 6 GPS. Will try this again and resubmit by Week 6 deadline (already received a strike, but want to figure this out)
