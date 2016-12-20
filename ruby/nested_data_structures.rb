1efdc39c3272f0a07d41e9f20a107a8bd6f2a1db
maxjhchoi <max.jh.choi@gmail.com>
2016-12-20 13:09:06 -0500

    Update nested_data_structures.rb

---
+++ ruby/nested_data_structures.rb
@@ -0,0 +1,12 @@
+building = {
+    floors: {
+        floor_1: "first_floor",
+        floor_2: "second_floor",
+        floor_3: "third_floor",
+        rooms: {
+          room_1: "room_1",
+          room_2: "room_2"
+          }
+    },
+    elevator: ["elevator_door", "elevator_shaft"]
+}
